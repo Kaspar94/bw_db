f44bce88dda57f379ebb473831b9762f120ef5e7
Kaspar <kaspar94@tdl.ee>
2019-07-08 14:58:00 +0300

    Db initialization file and flyway migrations

---
+++ init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE votes;
+
+ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY '1234';
+
+USE votes;
+
---
+++ sql/V2__Add_new_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `votes`.`votes` (
+ 
+`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` varchar(20)
+ 
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8; 
+
