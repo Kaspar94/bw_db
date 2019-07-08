6f84e2994416379336f7fedc1fce3e9097ecdb85
Kaspar <kaspar94@tdl.ee>
2019-07-08 17:30:16 +0300

    Table changes

--- init.sql
+++ init.sql
@@ -1,6 +1,10 @@
-CREATE DATABASE votes;
+CREATE DATABASE IF NOT EXISTS votes;
 
 ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY '1234';
+
+GRANT ALL PRIVILEGES ON * . * TO 'user'@'%';
+
+flush privileges;
 
 USE votes;
 
